96cfdec33ee3740ffe796befbc68de5e7f8ce3ad
Ondrej Valenta <ovalenta@spaneco.com>
2016-10-21 03:06:50 +0200

    EETData mappers, EETProvider first concepts Exceptions and Messages new resx files

---
+++ EETWrapper/Data/LogEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EETWrapper.Data
+{
+	public class LogEventArgs : EventArgs
+	{
+		public enum LogLevels { Info, Warn, Error,
+			Trace
+		}
+
+		public string Message { get; private set; }
+
+		public LogLevels LogLevel { get; private set; }
+
+		public LogEventArgs(string message, LogLevels logLevel)
+		{
+			LogLevel = logLevel;
+			Message = message;
+		}
+	}
+}
---
+++ EETWrapper/EETResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace EETWrapper
+{
+	public class EETResponse
+	{
+		
+	}
+}
