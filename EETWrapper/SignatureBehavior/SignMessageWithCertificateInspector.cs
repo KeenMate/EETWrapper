42521e724ed0226acf7b480ef4fc6420bbb6f361
Ondrej Valenta <me@ondrejvalenta.com>
2016-11-16 22:05:58 +0100

    Working solution again

--- EETWrapper/SignatureBehavior/SignMessageWithCertificateInspector.cs
+++ EETWrapper/SignatureBehavior/SignMessageWithCertificateInspector.cs
@@ -142,7 +142,7 @@ namespace EETWrapper.SignatureBehavior
 					//< wsse:Security xmlns:wsse = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns: wsu = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" soap: mustUnderstand = "1" >
 
 
-						 XmlElement security = doc.CreateElement("wsse", "Security", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+			XmlElement security = doc.CreateElement("wsse", "Security", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
 			security.SetAttribute("xmlns:wsu",
 				"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
 
